1ca58621df91e17423ba7858d5a5d9ccc6e04c6e
Shijikori <jeremi.campagna@gmail.com>
2022-04-21 13:44:47 -0400

    moved general channel search to universal function

--- bot.py
+++ bot.py
@@ -9,14 +9,20 @@ GUILD = os.getenv('GUILD_NAME')
 
 client = discord.Client()
 
+def findChannels(name:str):
+    chanlist = []
+    for guild in client.guilds:
+        for channel in guild.text_channels:
+            if channel.name == name:
+                chanlist.append(channel)
+    return chanlist
+
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-    general_list = []
-    for guild in client.guilds:
-        for channel in guild.text_channels:
-            if channel.name == 'general':
-                general_list.append(channel)
+    general_list = findChannels("general")
+    
     for channel in general_list:
         await channel.send('I am now online!')
 
