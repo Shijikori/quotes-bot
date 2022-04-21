8a6102ec27f4443e41990ef8ee569b5466966e11
Shijikori <jeremi.campagna@gmail.com>
2022-04-21 12:23:14 -0400

    added connection and message on connect

--- bot.py
+++ bot.py
@@ -2,3 +2,23 @@ import os
 import discord
 from dotenv import load_dotenv
 
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('GUILD_NAME')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+    general_list = []
+    for guild in client.guilds:
+        for channel in guild.text_channels:
+            if channel.name == 'general':
+                general_list.append(channel)
+    for channel in general_list:
+        await channel.send('I am now online!')
+
+
+client.run(TOKEN)
