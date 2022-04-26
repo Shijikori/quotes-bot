fe30562629b59bccc6bd3b683166a6c6ff1917e6
Shijikori <jeremi.campagna@gmail.com>
2022-04-26 14:47:48 -0400

    retreive quotes from message feature

--- bot.py
+++ bot.py
@@ -9,8 +9,11 @@ GUILD = os.getenv('GUILD_NAME')
 
 
 #intents
-intents = discord.Intents()
+intents = discord.Intents.default()
 intents.members = True
+intents.messages = True
+
+quotesChan = None
 
 client = discord.Client(intents=intents)
 
@@ -22,18 +25,43 @@ def findChannels(name:str):
                 chanlist.append(channel)
     return chanlist
 
-
 #events
 @client.event
 async def on_ready():
+    global quotesChan
     print(f'{client.user} has connected to Discord!')
     general_list = findChannels("general")
-    
+    quotesChan = findChannels("quotes")
+    print(f" {general_list} | {quotesChan}")
+
+
     for channel in general_list:
         await channel.send('I am now online!')
 
 @client.event
 async def on_member_join(member):
     await member.send(f'{member.name}, welcome to the most sophisticated discord server in the universe.')
+
+@client.event
+async def on_message(message):
+    global quotesChan
+    print("message received!")
+    if message.author == client.user:
+        print("author is me!")
+        return
+    print("author is not me!")
+    save = False
+    quote = ""
+    if message.channel.id == quotesChan[0].id:
+        print("message is a quote!")
+        for i in range(0,len(message.content)):
+            if save and not message.content[i] == '\"':
+                quote += message.content[i]
+            if message.content[i] == '\"' and save:
+                break
+            if message.content[i] == '\"':
+                save = True
+          
+        print(f"{message.guild} | {message.mentions[0]} | {quote}")
 
 client.run(TOKEN)
