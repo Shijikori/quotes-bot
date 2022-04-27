c69910c14e1fbd7fbdac72145e8bd6da8ccbfce3
gabrielb-l <gabrielbrochu.lessard@gmail.com>
2022-04-26 20:45:13 -0400

    added commands imp2met & met2imp

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,10 +8,12 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('GUILD_NAME')
 
+bot = commands.Bot(command_prefix='!')
 
 #intents
-intents = discord.Intents()
+intents = discord.Intents.default()
 intents.members = True
+intents.messages = True
 
 client = discord.Client(intents=intents)
 
@@ -35,5 +38,18 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     await member.send(f'{member.name}, welcome to the most sophisticated discord server in the universe.')
+
+@bot.command(name='imp2met', help='Converts inches to cms.')
+async def imp2met(ctx, measure):
+    print ("someone used this command")
+    metricmeasure = (measure*2.54)
+    response = (measure + ' is ' + metricmeasure + 'cm')
+    await ctx.send(response)
+
+@bot.command(name='met2imp', help='Converts cm to inches.')
+async def met2imp(ctx, measure):
+    imperialmeasure = (measure/2.54)
+    response = (measure + ' is ' + imperialmeasure + 'inches')
+    await ctx.send(response)
 
 client.run(TOKEN)
