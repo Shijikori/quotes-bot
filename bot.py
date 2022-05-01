83badb34a6f09c437ec66105a3aaf804c9420f79
Mikasu-X14 <jeremi.campagna@gmail.com>
2022-05-01 12:25:54 -0400

    removed unnecessary functions

--- bot.py
+++ bot.py
@@ -77,30 +77,6 @@ def queryDB(guildid, userid):
         conn.commit()
         return quotes
 
-#imperial to metric conversion command
-@client.command(name='imp2met', help='Converts inches to cms.')
-async def imp2met(ctx, measure):
-    try:
-        measure = float(measure)
-    except ValueError:
-        await ctx.send("Argument is not a number!")
-        return
-    metricmeasure = (measure*2.54)
-    response = f"{measure} inches is about {metricmeasure:.3f} centimeters"
-    await ctx.send(response)
-
-#metric to imperial conversion command
-@client.command(name='met2imp', help='Converts cm to inches.')
-async def met2imp(ctx, measure):
-    try:
-        measure = float(measure)
-    except ValueError:
-        await ctx.send("Argument is not a number!")
-        return
-    imperialmeasure = (measure/2.54)
-    response = f"{measure} centimeters is about {imperialmeasure:.3f} inches"
-    await ctx.send(response)
-
 #query command
 @client.command(name='query', help="Gets quote from mentionned user.")
 async def query(ctx, query:discord.Member):
@@ -117,17 +93,9 @@ async def query(ctx, query:discord.Member):
 async def on_ready():
     global quotesChan
     print(f'{client.user} has connected to Discord!')
-    general_list = findChannels("general")
     quotesChan = findChannels("quotes")
     for guild in client.guilds:
         createGuildTable(guild.id)
-
-    for channel in general_list:
-        await channel.send('I am now online!')
-
-@client.event
-async def on_member_join(member):
-    await member.send(f'{member.name}, welcome to the most sophisticated discord server in the universe.')
 
 @client.event
 async def on_message(message):
